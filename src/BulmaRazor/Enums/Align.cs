6279b06ab8c6ddba8488397144926670272846e0
loogn <loogn_0707@126.com>
2021-01-30 18:08:18 +0800

    更改

--- src/BulmaRazor/Enums/Align.cs
+++ src/BulmaRazor/Enums/Align.cs
@@ -9,8 +9,30 @@ namespace BulmaRazor.Components
         }
 
         public static Align Default = new Align("");
-        public static Align Center = new Align("is-centered");
+        public static Align Centered = new Align("is-centered");
         public static Align Right = new Align("is-right");
-        
+        public static Align Left = new Align("is-left");
+    }
+    public class AddonsAlign : EnumBase
+    {
+        private AddonsAlign(string value) : base(value)
+        {
+        }
+        public static AddonsAlign Default = new AddonsAlign("");
+
+
+        public static AddonsAlign Right = new AddonsAlign("has-addons-right");
+        public static AddonsAlign Centered = new AddonsAlign("has-addons-centered");
+    }
+
+    public class GroupedAlign : EnumBase
+    {
+        private GroupedAlign(string value) : base(value)
+        {
+        }
+        public static GroupedAlign Default = new GroupedAlign("");
+
+        public static GroupedAlign Right = new GroupedAlign("is-grouped-right");
+        public static GroupedAlign Centered = new GroupedAlign("is-grouped-centered");
     }
 }
