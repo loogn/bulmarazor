454ef22d1493b2e9765b4e27ea96d3b5cc13fbca
wangshenglong <loogn_0707@126.com>
2021-03-01 23:50:55 +0800

    整理Dialog

--- src/BulmaRazor/Components/Components/Modal/DialogOptions.cs
+++ src/BulmaRazor/Components/Components/Modal/DialogOptions.cs
@@ -1,4 +1,5 @@
 using System;
+using Microsoft.AspNetCore.Components;
 
 namespace BulmaRazor.Components
 {
@@ -9,9 +10,22 @@ namespace BulmaRazor.Components
         /// </summary>
         internal int Type { get; set; }
         public string Message { get; set; }
-        public string Title { get; set; }
-        public Action? Sure { get; set; }
-        public Action? Cancel { get; set; }
-        public Action<string>? Result { get; set; }
+        public string Title { get; set; } = "提示信息";
+        public bool ShowClose { get; set; }
+        public Action OK { get; set; }
+        public Action Cancel { get; set; }
+        public Action Close { get; set; }
+        /// <summary>
+        /// prompt value
+        /// </summary>
+        public Action<string> Result { get; set; }
+
+        public string Placeholder { get; set; } = "请输入内容";
+
+        public string OKText { get; set; } = "确定";
+        public string CancelText { get; set; } = "取消";
+        public Color OKColor { get; set; } = Color.Primary;
+        public Color CancelColor { get; set; } = Color.Default;
+
     }
 }
