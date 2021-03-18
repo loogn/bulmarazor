b1b18ab9bffc6a5f2a6dd35587c7021905b2d590
loogn <loogn_0707@126.com>
2021-03-18 10:45:53 +0800

    colorpicker

--- src/BulmaRazor/wwwroot/jsplugin/jscolor/jscolor-export.js
+++ src/BulmaRazor/wwwroot/jsplugin/jscolor/jscolor-export.js
@@ -1,6 +1,16 @@
 ;
-export function init(selector,options){
-    
-    var obj= new JSColor(selector,options);
+
+export function init(selector, options) {
+
+    // options.onChange = function () {
+    //     console.log(this);
+    //     //this is jscolor instance
+    // }
+    // options.onInput = function () {
+    //     console.log(this);
+    //     //this is jscolor instance
+    //     // DotNet.invokeMethodAsync("BulmaRazor","JSCallback",datepicker.data.element.id,"onInput",[datepicker.data.value()]);
+    // }
+    var obj = new JSColor(selector, options);
     return obj;
 }
