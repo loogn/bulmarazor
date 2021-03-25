b8f729ba0bf9a70e49e18a8f683c2ede40d3170e
loogn <loogn_0707@126.com>
2021-03-25 18:10:03 +0800

    Cascader基本完成

---
+++ src/BulmaRazor/Components/Form/Cascader/Cascader.razor.cs
@@ -0,0 +1,321 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BulmaRazor.Utils;
+using Microsoft.AspNetCore.Components;
+
+namespace BulmaRazor.Components
+{
+    public partial class Cascader<TValue>
+    {
+        [Inject] private BulmaRazorJsInterop JsInterop { get; set; }
+
+        readonly string Id = "cascader_" + Guid.NewGuid().ToString("N");
+
+        private bool hoverInput;
+
+        string classes => CssBuilder.Default("cascader")
+            .AddClassFromAttributes(Attributes)
+            .Build();
+
+        private string dropDownCls => CssBuilder.Default("cascader-dropdown")
+            .AddClass(B.Hidden, !isShow)
+            .Build();
+
+        private string tagsInputCls => CssBuilder.Default("tags-input")
+            .AddClass(B.Clickable)
+            .AddClass("is-small", IsSmall)
+            .AddClass("is-normal", IsNormal)
+            .AddClass("is-medium", IsMedium)
+            .AddClass("is-large", IsLarge)
+            .Build();
+
+        private string GetItemCls(CascaderItem<TValue> item)
+        {
+            return CssBuilder.Default()
+                .AddClass("active", item.IsSelected)
+                .AddClass("is-disabled", item.Disabled)
+                .Build();
+        }
+
+        private bool GetItemChecked(CascaderItem<TValue> item)
+        {
+            if (item.Children.Any())
+            {
+                return item.Children.All(x => x.IsChecked);
+            }
+            else
+            {
+                return item.IsChecked;
+            }
+        }
+
+        private string GetTagCls()
+        {
+            if (IsRoundedTag)
+            {
+                return "tag is-rounded";
+            }
+            else
+            {
+                return "tag";
+            }
+        }
+
+        private bool isShow = false;
+
+        private HashSet<CascaderItem<TValue>> SelectedList = new();
+
+        [Parameter] public bool IsSmall { get; set; }
+        [Parameter] public bool IsNormal { get; set; }
+        [Parameter] public bool IsMedium { get; set; }
+        [Parameter] public bool IsLarge { get; set; }
+        [Parameter] public bool Clearable { get; set; }
+        [Parameter] public bool IsMultiple { get; set; }
+        [Parameter] public bool IsIsolated { get; set; }
+        [Parameter] public bool IsRoundedTag { get; set; }
+        [Parameter] public string Separator { get; set; } = "/";
+
+        private IEnumerable<CascaderItem<TValue>> dataView = new List<CascaderItem<TValue>>();
+        [Parameter] public IEnumerable<CascaderItem<TValue>> Data { get; set; }
+
+        [Parameter] public string Placeholder { get; set; } = "请选择";
+        [Parameter] public bool ShowAllLevels { get; set; } = true;
+        [Parameter] public bool IsCollapseTags { get; set; }
+        [Parameter] public RenderFragment<CascaderItem<TValue>> ItemSlot { get; set; }
+
+        [Parameter] public EventCallback<Cascader<TValue>> OnChange { get; set; }
+        [Parameter] public TValue Value { get; set; }
+        [Parameter] public EventCallback<TValue> ValueChanged { get; set; }
+
+        [Parameter] public HashSet<TValue> Values { get; set; }
+        [Parameter] public EventCallback<HashSet<TValue>> ValuesChanged { get; set; }
+
+        private async Task Fire()
+        {
+            if (IsMultiple)
+            {
+                Values = new HashSet<TValue>(SelectedList.Select(x => x.Value));
+                await ValuesChanged.InvokeAsync(Values);
+            }
+            else
+            {
+                var item = SelectedList.FirstOrDefault();
+                var vv = item == null ? default : item.Value;
+                await ValueChanged.InvokeAsync(vv);
+            }
+
+            await OnChange.InvokeAsync(this);
+        }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            await base.OnAfterRenderAsync(firstRender);
+            if (firstRender)
+            {
+                await JsInterop.BindClickWithoutSelf(Id);
+            }
+        }
+        
+        private void CheckValues(CascaderItem<TValue> item, HashSet<TValue> values)
+        {
+            if (values.Contains(item.Value))
+            {
+                item.IsChecked = true;
+                if (IsIsolated || item.Children.Count == 0)
+                {
+                    SelectedList.Add(item);
+                }
+            }
+            foreach (var subItem in item.Children)
+            {
+                CheckValues(subItem, values);
+            }
+        }
+
+        protected override Task OnInitializedAsync()
+        {
+            if (Data != null)
+            {
+                HashSet<TValue> values = null;
+                if (IsMultiple && Values != null && Values.Any())
+                {
+                    values = Values;
+                }
+                else if (Value != null)
+                {
+                    values = new HashSet<TValue>() {Value};
+                }
+
+                if (values != null)
+                {
+                    foreach (var item in Data)
+                    {
+                        CheckValues(item, values);
+                    }
+                }
+
+                dataView = Data;
+            }
+
+            JSCallbackManager.AddEventHandler(Id, "clickWithoutSelf", new Action(() =>
+            {
+                isShow = false;
+                StateHasChanged();
+            }));
+            return base.OnInitializedAsync();
+        }
+
+        protected override ValueTask DisposeAsync(bool disposing)
+        {
+            JSCallbackManager.DisposeObject(Id);
+            return base.DisposeAsync(disposing);
+        }
+
+        public void Toggle()
+        {
+            isShow = !isShow;
+        }
+
+        private string GetText(CascaderItem<TValue> item)
+        {
+            if (item == null) return string.Empty;
+
+            if (!ShowAllLevels) return item.Text;
+
+            List<string> linkList = new List<string>();
+            var cur = item;
+            do
+            {
+                linkList.Add(cur.Text);
+                cur = cur.Parent;
+            } while (cur != null);
+
+            linkList.Reverse();
+            return String.Join(" " + Separator + " ", linkList);
+        }
+
+        private async Task SelectOne(IEnumerable<CascaderItem<TValue>> list, CascaderItem<TValue> item)
+        {
+            if (item.Disabled) return;
+
+            if (IsMultiple && item.Children.Count == 0) return;
+
+
+            foreach (var cascaderItem in list)
+            {
+                cascaderItem.IsSelected = cascaderItem == item;
+                cascaderItem.Children.ForEach(x => x.IsSelected = false);
+            }
+
+            if (IsIsolated && !IsMultiple) return;
+
+            if (item.Children.Count == 0)
+            {
+                SelectedList.Clear();
+                SelectedList.Add(item);
+                isShow = false;
+                await Fire();
+                
+            }
+        }
+
+
+        private void CheckChildren(CascaderItem<TValue> item)
+        {
+            item.IsChecked = true;
+            if (item.Children.Count == 0)
+            {
+                SelectedList.Add(item);
+            }
+            else
+            {
+                foreach (var subItem in item.Children)
+                {
+                    CheckChildren(subItem);
+                }
+            }
+        }
+
+        private void UnCheckChildren(CascaderItem<TValue> item)
+        {
+            item.IsChecked = false;
+            SelectedList.Remove(item);
+
+            foreach (var subItem in item.Children)
+            {
+                UnCheckChildren(subItem);
+            }
+        }
+
+        private async Task Check(CascaderItem<TValue> item, bool cked)
+        {
+            if (IsIsolated || item.Children.Count == 0)
+            {
+                if (item.IsChecked)
+                {
+                    SelectedList.Remove(item);
+                    item.IsChecked = false;
+                    item.IsSelected = false;
+                }
+                else
+                {
+                    SelectedList.Add(item);
+                    item.IsChecked = true;
+                    item.IsSelected = true;
+                }
+            }
+            else
+            {
+                if (cked)
+                {
+                    //取消
+                    UnCheckChildren(item);
+                }
+                else
+                {
+                    //全选
+                    CheckChildren(item);
+                }
+            }
+
+            await Fire();
+        }
+
+        private async Task CheckRadio(CascaderItem<TValue> item)
+        {
+            SelectedList.Clear();
+            SelectedList.Add(item);
+            item.IsChecked = true;
+            item.IsSelected = true;
+            await Fire();
+        }
+
+        private async Task Remove(CascaderItem<TValue> item)
+        {
+            item.IsSelected = false;
+            item.IsChecked = false;
+            SelectedList.Remove(item);
+            await Fire();
+        }
+
+        public async Task ClearSelectedNodes()
+        {
+            foreach (var item in SelectedList)
+            {
+                item.IsSelected = false;
+                item.IsChecked = false;
+            }
+
+            SelectedList.Clear();
+            await Fire();
+        }
+
+        public HashSet<CascaderItem<TValue>> GetSelectedNodes()
+        {
+            return SelectedList;
+        }
+
+    }
+}
---
+++ src/BulmaRazor/Components/Form/Cascader/CascaderItem.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+
+namespace BulmaRazor.Components
+{
+    public class CascaderItem<TValue>
+    {
+        public TValue Value { get; set; }
+        public string Text { get; set; }
+        /// <summary>
+        /// 如果选中，子项都不选
+        /// </summary>
+        internal bool IsSelected { get; set; }
+        internal bool IsChecked { get; set; }
+        public bool Disabled { get; set; }
+        
+        public CascaderItem<TValue> Parent { get; set; }
+        public List<CascaderItem<TValue>> Children { get; set; } = new();
+
+        public void AddItem(CascaderItem<TValue> item)
+        {
+            item.Parent = this;
+            Children.Add(item);
+        }
+
+        public void AddItem(TValue value, string text)
+        {
+            var item = new CascaderItem<TValue>() {Value = value, Text = text};
+            AddItem(item);
+        }
+        
+    }
+}
